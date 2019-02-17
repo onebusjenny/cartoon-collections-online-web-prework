6c7fc7e08dece31b1cfe180826ce151215fe8ae8
Jenny Chu <jchudesign0930@gmail.com>
2019-02-17 07:14:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name)
+  name = ["Doc","Dopey","Bashful","Grumpy"]
+  name.each do |name|
+    roll_call_dwarves (name)
+    
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  planeteer_calls << "!"
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words = ["puff", "go", "two"]
+  
+  short_words.select do |short_words|
+  
 end
 
 def find_the_cheese# code an argument here
