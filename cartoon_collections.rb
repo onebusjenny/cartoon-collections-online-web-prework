870eb6b7cc1b7384bc281b2823e4a08e3d6a5caa
Jenny Chu <jchudesign0930@gmail.com>
2019-02-17 07:20:22 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name)
+  name = ["Doc","Dopey","Bashful","Grumpy"]
+  name.each do |name|
+    roll_call_dwarves (name)
+    
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  planeteer_calls << "!"
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words = ["puff", "go", "two"]
+  short_words.any? do |short_word|
+  short_word.length > 4
 end
+  
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_type.include?("chedder")
 end
